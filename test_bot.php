30f315dc9d9789b9c4f33215da2c279a3409f1d7
HEX <kamtorn.ku@gmail.com>
2018-06-26 11:42:56 +0700

    reply

--- test_bot.php
+++ test_bot.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'be0TDkkZ2drjjPacz274jNhbuO1hb3bVOVNgzPZkfis4FQFW9r5ydZj0FFN5fwLb0S6pLaXwL/s/i62DoabJomYXsAl2kxa+wH3a+N2t83PnjuQKR2BiJtGZRcta74hPF6/+d/o0TUIrskxyq9PgiwdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'DGuuVccwqxYn+uroRdkiLBkYddWELZNxA03jZ36rKVwFpjutP9c56kLU8J9+Mf1/aS9Q4PoGYUEj7RKhjaIIoel4wRL35bQgbijVJBRe2LtiCZg3WX2VStSqATXMYVtSbowItcUApiUvxqKt1jqSTQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
